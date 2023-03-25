7af7f114655be46f5a06bcf3c85499a14d7ff873
anastasia-iteric <anastasia.kyrylenko@iteric.com>
2023-03-25 11:19:00 +0100

    feat: Admin login form

--- src/containers/admin-panel/index.tsx
+++ src/containers/admin-panel/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
+import { ADMIN_PASSWORD } from '../../constants';
 import { getProducts } from '../../services/admin-panel-service/admin-panel.service';
 import { IProduct } from '../../services/admin-panel-service/types';
+import { AdminLoginForm } from './components/admin-login-form';
 import { NavBar } from './components/nav-bar';
 import { ProductCards } from './components/product-cards';
 
@@ -9,6 +11,7 @@ import './style.css';
 export const AdminPanel = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState(localStorage.getItem('admin-password'));
 
   const fetchProducts = async (isRefetching?: boolean) => {
     !isRefetching && setIsLoading(true);
@@ -17,9 +20,20 @@ export const AdminPanel = () => {
     !isRefetching && setIsLoading(false);
   };
 
+  const handleSetPassword = (password: string) => () => {
+    if (password === ADMIN_PASSWORD) {
+      localStorage.setItem('admin-password', password);
+      setPassword(password);
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  if (password !== ADMIN_PASSWORD) {
+    return <AdminLoginForm handleSetPassword={handleSetPassword} />;
+  }
 
   return (
     <div className="admin-pannel-wrapper">
