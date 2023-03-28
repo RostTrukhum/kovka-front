f9027d08ee55a968b8ee3ca346c328fca292c94a
anastasia-iteric <anastasia.kyrylenko@iteric.com>
2023-03-28 14:26:03 +0200

    fix: Change backend link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = 'http://localhost:3000';
-// export const BACKEND_URL = 'https://gospodar-kovka.onrender.com';
+// export const BACKEND_URL = 'http://localhost:3000';
+export const BACKEND_URL = 'https://gospodar-kovka.onrender.com';
 export const UPLOAD_PHOTO_BACKEND = 'https://api.imgbb.com/1/upload';
 export const UPLOAD_PHOTO_BACKEND_KEY = '522594ff544734f4098d95500e05ee54';
 export const FETCH_PRODUCT_LIMIT = 6;
