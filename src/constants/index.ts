b68c87c5983b3598661d16a80a6fddf7d0317123
anastasia-iteric <anastasia.kyrylenko@iteric.com>
2023-03-29 20:06:02 +0200

    Change password

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,4 @@ export const BACKEND_URL = 'https://gospodar-kovka.onrender.com';
 export const UPLOAD_PHOTO_BACKEND = 'https://api.imgbb.com/1/upload';
 export const UPLOAD_PHOTO_BACKEND_KEY = '522594ff544734f4098d95500e05ee54';
 export const FETCH_PRODUCT_LIMIT = 6;
-export const ADMIN_PASSWORD = 'r14031979';
+export const ADMIN_PASSWORD = 'Rost_333';
