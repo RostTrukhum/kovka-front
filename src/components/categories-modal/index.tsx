981cf2ac86abb30c246965fb5289f62e01be4b99
Rostyslav <rtrukhym@go7.io>
2023-11-26 18:33:41 +0200

    feat: Overlay for categories

--- src/components/categories-modal/index.tsx
+++ src/components/categories-modal/index.tsx
@@ -36,57 +36,60 @@ export const CategoriesModal = ({ isVisible, onClose }: ICategoriesModalProps) =
   };
 
   return (
-    <div className={`categories-modal-wrapper ${isVisible && 'categories-modal-wrapper-opened'}`}>
-      <div className="categories-modal-title-wrapper">
-        <EmptyLeftArrowIcon onClick={onClose} cursor={'pointer'} width={20} height={20} />
-        <p className="categories-modal-title">Каталог</p>
-      </div>
-      <div className="categories-modal-content-wrapper">
-        <div>
-          {productTypes.map(({ title, subtypes, _id, type }) => (
-            <ul key={_id} className="categories-modal-category-list-wrapper">
-              <div
-                onClick={handleAddActiveCategory(title)}
-                className="categories-modal-category-list-title"
-              >
-                {title}
-                <EmptyRightArrowIcon
-                  className={`categories-modal-category-list-item-arrow ${
-                    handleShowSubcategoriesList(title) &&
-                    'categories-modal-category-list-item-arrow-opened'
-                  }`}
-                  width={10}
-                  height={10}
-                />
-              </div>
-              {handleShowSubcategoriesList(title) && (
-                <li
-                  key={_id}
-                  onClick={handleNavigateCategory(type, 'all')}
-                  className="categories-modal-category-list-item"
+    <>
+      <div className={`categories-modal-wrapper ${isVisible && 'categories-modal-wrapper-opened'}`}>
+        <div className="categories-modal-title-wrapper">
+          <EmptyLeftArrowIcon onClick={onClose} cursor={'pointer'} width={20} height={20} />
+          <p className="categories-modal-title">Каталог</p>
+        </div>
+        <div className="categories-modal-content-wrapper">
+          <div>
+            {productTypes.map(({ title, subtypes, _id, type }) => (
+              <ul key={_id} className="categories-modal-category-list-wrapper">
+                <div
+                  onClick={handleAddActiveCategory(title)}
+                  className="categories-modal-category-list-title"
                 >
-                  Усі продукти
-                </li>
-              )}
-
-              {handleShowSubcategoriesList(title) &&
-                subtypes.map(({ title, _id, subtype }) => (
+                  {title}
+                  <EmptyRightArrowIcon
+                    className={`categories-modal-category-list-item-arrow ${
+                      handleShowSubcategoriesList(title) &&
+                      'categories-modal-category-list-item-arrow-opened'
+                    }`}
+                    width={10}
+                    height={10}
+                  />
+                </div>
+                {handleShowSubcategoriesList(title) && (
                   <li
                     key={_id}
-                    onClick={handleNavigateCategory(type, subtype)}
+                    onClick={handleNavigateCategory(type, 'all')}
                     className="categories-modal-category-list-item"
                   >
-                    {title}
+                    Усі продукти
                   </li>
-                ))}
-            </ul>
-          ))}
-        </div>
-        <div className="categories-modal-messangers">
-          <NavPhoneNumbers />
-          <Messangers isRow />
+                )}
+
+                {handleShowSubcategoriesList(title) &&
+                  subtypes.map(({ title, _id, subtype }) => (
+                    <li
+                      key={_id}
+                      onClick={handleNavigateCategory(type, subtype)}
+                      className="categories-modal-category-list-item"
+                    >
+                      {title}
+                    </li>
+                  ))}
+              </ul>
+            ))}
+          </div>
+          <div className="categories-modal-messangers">
+            <NavPhoneNumbers />
+            <Messangers isRow />
+          </div>
         </div>
       </div>
-    </div>
+      {isVisible && <div onClick={onClose} className="categories-modal-overlay" />}
+    </>
   );
 };
